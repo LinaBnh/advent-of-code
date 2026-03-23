3c417e90d4289e374d9534b8448d0c3dcdb13834
lina.bnh23@gmail.com <lina.bnh23@gmail.com>
2026-03-23 13:48:26 +0100

    day 1 solved

--- day1/day1_a.cpp
+++ day1/day1_a.cpp
@@ -8,16 +8,18 @@ int main(){
     std::ifstream file("input.txt");
     std::string line;
 
-    int position=50;
-    int count=0;
+    int position=50; //dial starts at 50
+    int count=0; //number of times the dial lands on 0
 
     while (std::getline(file, line)) {
         if (line.empty()) continue;
 
-        char direction = line[0];
-        int rotation = std::stoi(line.substr(1));
+        char direction = line[0]; //left or right
+        int rotation = std::stoi(line.substr(1)); //number of clicks converted from string into int
 
+        //the dial has 100 positions (0-99) so i used modulo 100 to wrap around
         if (direction == 'L')
+            //+100 before % handles negative values when going left
             position = ((position - rotation) % 100 + 100) % 100;
         else 
             position = (position + rotation) % 100;
--- day1/day1_b.cpp
+++ day1/day1_b.cpp
@@ -4,19 +4,22 @@
 
 using namespace std;
 
-//fonction pour compter les passages par 0 pour une rotation
-
+//function to count how many times the dial passes through 0 during a rotation
+//idea: split the rotation into full loops (multiples of 100) + remaining rotations
 int countClicks(int position, int rotation, char direction){
-    // un tour complet de 100 clics passe forcément par 0 une fois
-    int clicks = rotation/100;
+    // each full loop of 100 clicks always passes through 0 once
+    int clicks = rotation / 100;
     
-    // pour les tours qui ne sont pas complet
-    int tour = rotation % 100;
+    // remaining clicks after removing full loops
+    int remain = rotation % 100;
 
     if (direction == 'L')
-        clicks += (position == 0) ? 0 : (tour >= position);
+        //if already on 0, it wont cross 0 again until nest full loop
+        //otherwise, check if the remain in enough to reach 0
+        clicks += (position == 0) ? 0 : (remain >= position);
     else
-        clicks += (tour >= (100 - position));
+        //check if the remain is enough to cross 0 (distance to 0 going right = 100 - position)
+        clicks += (remain >= (100 - position));
 
     return clicks;
 }
@@ -25,18 +28,20 @@ int main(){
     ifstream file("input.txt");
     string line;
 
-    int position=50;
-    int count=0;
+    int position=50; //dial starts at 50
+    int count=0; //number of times the dial lands on 0
 
     while (getline(file,line)){
         if (line.empty()) continue;
 
-        char direction = line[0];
-        int rotation = stoi(line.substr(1));
+        char direction = line[0]; //left or right
+        int rotation = stoi(line.substr(1)); //number of clicks converted from string into int
 
         count += countClicks(position, rotation, direction);
 
+        //update final position, wrapping around 0-99
         if (direction == 'L')
+            //+100 before % handles negative values when going left
             position = ((position - rotation) % 100 + 100) % 100;
         else
             position = (position + rotation) % 100;
