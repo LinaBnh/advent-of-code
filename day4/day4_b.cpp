5cc1dc93ef265f6cbb5b3f29a3acde64fc7cf091
lina.bnh23@gmail.com <lina.bnh23@gmail.com>
2026-03-23 18:23:57 +0100

    day 4 solved

--- day4/day4_b.cpp
+++ day4/day4_b.cpp
@@ -6,13 +6,10 @@
 using namespace std;
 
 //function to count the accessible papers
-//for each '@', we increment the counter of its 8 neighbors then count rolls with < than 4
-int countPapers(const vector<string>& grid){
+//for each '@' we increment the counter of its 8 neighbors
+void countPapers(vector<vector<int>>& counter, const vector<string>& grid){
     int rows = grid.size();
     int cols = grid[0].size();
-    int papers = 0;
-
-    vector<vector<int>> counter(rows, vector<int>(cols, 0));
 
     for (int i=1; i<rows-1; i++){
         for (int j=1; j<cols-1; j++){
@@ -27,14 +24,23 @@ int countPapers(const vector<string>& grid){
             counter[i+1][j+1]++;
         }
     }
+}
+
+//added a function to remove accessible rolls and return how many were removed
+int removePapers(const vector<vector<int>>& counter, vector<string>& grid){
+    int rows = grid.size();
+    int cols = grid[0].size();
+    int removed = 0;
 
     for (int i=1; i<rows-1; i++){
         for (int j=1; j<cols-1; j++){
-            if (grid[i][j]=='@' && counter[i][j]<4) papers++;
+            if(grid[i][j]=='@' && counter[i][j]<4){
+                grid[i][j] = '.';
+                removed++;
+            }
         }
     }
-
-    return papers;
+    return removed;
 }
 
 int main(){
@@ -51,7 +57,21 @@ int main(){
     grid[0] = string(grid[1].size(), '.');
     grid.push_back(grid[0]);
 
-    cout << "Solution part1: " << countPapers(grid) << endl;
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    int papers = 0;
+    int removed = 0;
+
+    do {
+        //counter must be reset at each iteration since the grid has changed
+        vector<vector<int>> counter(rows, vector<int>(cols, 0));
+        countPapers(counter, grid);
+        removed = removePapers(counter, grid);
+        papers += removed;
+    } while (removed > 0);
+
+    cout << "Solution part2: " << papers << endl;
 
     return 0;
 }
